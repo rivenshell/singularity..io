fbc421608ea5c311b360c4233490f9f1e039f787
Rivenshell <rivenshell111@gmail.com>
2022-12-02 15:54:53 -0500

    Loading page Update

--- interface/src/LoadingSpinner.js
+++ interface/src/LoadingSpinner.js
@@ -15,7 +15,7 @@ const useStyles = () => ({
     }
 });
 
-const processingSteps = ['Generating images 👨🏽‍🎨', 'Doing fancy calculations '];
+const processingSteps = ['Generating images 👨🏽‍🎨', 'AI creating neural connections'];
 
 const LoadingSpinner = ({classes, isLoading}) => {
     const [textIdx, setTextIdx] = useState(0);
